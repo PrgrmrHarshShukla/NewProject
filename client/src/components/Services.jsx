29d8d43d6175c7e569ec66f58892e84142df1c5e
HARSH SHUKLA <ashuklaharsh2005@gmail.com>
2023-06-04 18:48:46 +0530

    More modifications

--- client/src/components/Services.jsx
+++ client/src/components/Services.jsx
@@ -4,7 +4,7 @@ import '@fortawesome/fontawesome-free/css/all.css';
 function Services() {
    return (
       <div className="w-[80vw] ml-[20vw] justify-center h-screen flex flex-col  items-center">
-         <div className="flex flex-row gap-2 justify-start border-b-2 border-gray-400 mb-4 w-[62vw]">
+         <div className="flex flex-row gap-2 justify-start border-b-2 border-gray-400 mb-12 w-[62vw] mt-[5vh]">
             <h2 className="text-orange-500">
                <i className="fas fa-briefcase"></i>
             </h2>
@@ -12,58 +12,29 @@ function Services() {
          </div>
 
          <div className="w-[70vw] h-[73vh] flex flex-col  items-center gap-2">
-            <div className="flex flex-row flex-wrap gap-4">
-               <img
-                  className="d-block w-[20vw] min-w-[100px] h-[20vh] hover:border-4 rounded-[10px] hover:border-red-500"
-                  src="https://images.unsplash.com/photo-1469474968028-56623f02e42e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bmF0dXJlfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60"
-                  alt="First slide"
-               />
-               <img
-                  className="d-block w-[20vw] min-w-[100px] h-[20vh] hover:border-4 rounded-[10px] hover:border-red-500"
-                  src="https://images.unsplash.com/photo-1426604966848-d7adac402bff?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8bmF0dXJlfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60"
-                  alt="Second slide"
-               />
-               <img
-                  className="d-block w-[20vw] min-w-[100px] h-[20vh] hover:border-4 rounded-[10px] hover:border-red-500"
-                  src="https://images.unsplash.com/photo-1472214103451-9374bd1c798e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8bmF0dXJlfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60"
-                  alt="Second slide"
-               />
-            </div>
+            
           
-            <div className="flex flex-row flex-wrap gap-4">
-               <img
-                  className="d-block w-[20vw] min-w-[100px] h-[20vh] hover:border-4 rounded-[10px] hover:border-red-500"
-                  src="https://images.unsplash.com/photo-1469474968028-56623f02e42e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bmF0dXJlfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60"
-                  alt="First slide"
-               />
-               <img
-                  className="d-block w-[20vw] min-w-[100px] h-[20vh] hover:border-4 rounded-[10px] hover:border-red-500"
-                  src="https://images.unsplash.com/photo-1426604966848-d7adac402bff?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8bmF0dXJlfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60"
-                  alt="Second slide"
-               />
-               <img
-                  className="d-block w-[20vw] min-w-[100px] h-[20vh] hover:border-4 rounded-[10px] hover:border-red-500"
-                  src="https://images.unsplash.com/photo-1472214103451-9374bd1c798e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8bmF0dXJlfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60"
-                  alt="Second slide"
-               />
-            </div>
+            
           
-            <div className="flex flex-row flex-wrap gap-4">
+            <div className="flex flex-col flex-wrap gap-4 justify-center items-center">
+              
                <img
-                  className="d-block w-[20vw] min-w-[100px] h-[20vh] hover:border-4 rounded-[10px] hover:border-red-500"
-                  src="https://images.unsplash.com/photo-1469474968028-56623f02e42e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bmF0dXJlfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60"
+                  className="d-block w-[55vw] min-w-[100px] h-[35vh]"
+                  src="src\assets\slider2.png"
                   alt="First slide"
                />
-               <img
-                  className="d-block w-[20vw] min-w-[100px] h-[20vh] hover:border-4 rounded-[10px] hover:border-red-500"
-                  src="https://images.unsplash.com/photo-1426604966848-d7adac402bff?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8bmF0dXJlfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60"
-                  alt="Second slide"
-               />
-               <img
-                  className="d-block w-[20vw] min-w-[100px] h-[20vh] hover:border-4 rounded-[10px] hover:border-red-500"
-                  src="https://images.unsplash.com/photo-1472214103451-9374bd1c798e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8bmF0dXJlfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60"
-                  alt="Second slide"
-               />
+               <div className="flex flex-row flex-wrap gap-4">
+                  <img
+                     className="d-block w-[32vw] min-w-[100px] h-[35vh] hover:border-4 rounded-[10px] hover:border-red-500"
+                     src="src\assets\slider3.png"
+                     alt="First slide"
+                  />
+                  <img
+                     className="d-block w-[32vw] min-w-[100px] h-[35vh] hover:border-4 rounded-[10px] hover:border-red-500"
+                     src="src\assets\slider1.png"
+                     alt="Second slide"
+                  />
+               </div>
             </div>
           
            
--- client/src/components/SmallSlider.jsx
+++ client/src/components/SmallSlider.jsx
@@ -11,17 +11,17 @@ function SmallSlider() {
           <div className="flex flex-row justify-center items-center gap-4">
           <img
             className="d-block min-w-[21vw] rounded-[10px] h-[25vh]"
-            src="https://images.unsplash.com/photo-1469474968028-56623f02e42e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bmF0dXJlfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60"
+            src="src\assets\slider1.png"
             alt="First slide"
           />
           <img
             className="d-block min-w-[21vw] rounded-[10px] h-[25vh]"
-            src="https://images.unsplash.com/photo-1426604966848-d7adac402bff?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8bmF0dXJlfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60"
+            src="src\assets\slider2.png"
             alt="Second slide"
           />
           <img
             className="d-block min-w-[21vw] rounded-[10px] h-[25vh]"
-            src="https://images.unsplash.com/photo-1472214103451-9374bd1c798e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8bmF0dXJlfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60"
+            src="src\assets\slider3.png"
             alt="Second slide"
           />
           </div>
@@ -31,17 +31,17 @@ function SmallSlider() {
           <div className="flex flex-row justify-center items-center gap-4">
           <img
             className="d-block min-w-[21vw] rounded-[10px] h-[25vh]"
-            src="https://images.unsplash.com/photo-1469474968028-56623f02e42e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bmF0dXJlfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60"
+            src="src\assets\slider1.png"
             alt="First slide"
           />
           <img
             className="d-block min-w-[21vw] rounded-[10px] h-[25vh]"
-            src="https://images.unsplash.com/photo-1426604966848-d7adac402bff?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8bmF0dXJlfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60"
+            src="src\assets\slider2.png"
             alt="Second slide"
           />
           <img
             className="d-block min-w-[21vw] rounded-[10px] h-[25vh]"
-            src="https://images.unsplash.com/photo-1472214103451-9374bd1c798e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8bmF0dXJlfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60"
+            src="src\assets\slider3.png"
             alt="Second slide"
           />
           </div>
@@ -51,43 +51,23 @@ function SmallSlider() {
           <div className="flex flex-row justify-center items-center gap-4">
           <img
             className="d-block min-w-[21vw] rounded-[10px] h-[25vh]"
-            src="https://images.unsplash.com/photo-1469474968028-56623f02e42e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bmF0dXJlfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60"
+            src="src\assets\slider1.png"
             alt="First slide"
           />
           <img
             className="d-block min-w-[21vw] rounded-[10px] h-[25vh]"
-            src="https://images.unsplash.com/photo-1426604966848-d7adac402bff?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8bmF0dXJlfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60"
+            src="src\assets\slider2.png"
             alt="Second slide"
           />
           <img
             className="d-block min-w-[21vw] rounded-[10px] h-[25vh]"
-            src="https://images.unsplash.com/photo-1472214103451-9374bd1c798e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8bmF0dXJlfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60"
+            src="src\assets\slider3.png"
             alt="Second slide"
           />
           </div>
         </Carousel.Item>
         
-        <Carousel.Item>
-          <div className="flex flex-row justify-center items-center gap-4">
-          <img
-            className="d-block min-w-[21vw] rounded-[10px] h-[25vh]"
-            src="https://images.unsplash.com/photo-1469474968028-56623f02e42e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bmF0dXJlfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60"
-            alt="First slide"
-          />
-          <img
-            className="d-block min-w-[21vw] rounded-[10px] h-[25vh]"
-            src="https://images.unsplash.com/photo-1426604966848-d7adac402bff?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8bmF0dXJlfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60"
-            alt="Second slide"
-          />
-          <img
-            className="d-block min-w-[21vw] rounded-[10px] h-[25vh]"
-            src="https://images.unsplash.com/photo-1472214103451-9374bd1c798e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8bmF0dXJlfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60"
-            alt="Second slide"
-          />
-          </div>
-        </Carousel.Item>
         
-
         
         
       </Carousel>
